8c881f1ded8462d024ed470d56ca30e832f06424
Vinay Maurya <vinaymaurya1781@gmail.com>
2024-08-20 13:23:26 +0530

    first commit

---
+++ marathon.js
@@ -0,0 +1,24 @@
+function updateCountdown() {
+    const eventDate = new Date('August 13, 2024 09:00:00').getTime();
+    const now = new Date().getTime();
+    const distance = eventDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
+
+    if (distance < 0) {
+        clearInterval(interval);
+        document.getElementById('timer').innerHTML = 'Event has started!';
+    }
+}
+
+const interval = setInterval(updateCountdown, 1000);
+
+window.onload = updateCountdown;
